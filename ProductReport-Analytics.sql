b70c5bd8be47c7b19676883e868dfa7c800d0510
Ganwari Nakathige Sakila Lakmal Nandasiri CB014383 <164737611+Sakilalakmal@users.noreply.github.com>
2026-02-26 15:24:57 +0530

    customer report and product report

---
+++ ProductReport-Analytics.sql
@@ -0,0 +1,73 @@
+CREATE VIEW gold.report_products AS WITH CTE_base_products AS (
+	SELECT
+		f.order_number,
+		f.order_date,
+		f.customer_key,
+		f.sales_amount,
+		f.quantity,
+		p.product_key,
+		p.product_name,
+		p.category,
+		p.subcategory,
+		p.cost
+	FROM
+		gold.fact_sales f
+		LEFT JOIN gold.dim_products p ON f.product_key = p.product_key
+	WHERE
+		order_date IS NOT NULL -- only considering valid data
+),
+product_aggregations AS (
+	SELECT
+		product_key,
+		product_name,
+		category,
+		subcategory,
+		cost,
+		DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS lifespan,
+		MAX(order_date) AS last_sale_date,
+		COUNT(DISTINCT order_number) AS total_orders,
+		COUNT(DISTINCT customer_key) AS total_customers,
+		SUM(sales_amount) AS total_sales,
+		SUM(quantity) AS total_quantity,
+		ROUND(
+			AVG(CAST(sales_amount AS FLOAT) / NULLIF(quantity, 0)),
+			1
+		) AS avg_selling_price
+	FROM
+		CTE_base_products
+	GROUP BY
+		product_key,
+		product_name,
+		category,
+		subcategory,
+		cost
+)
+SELECT
+	product_key,
+	product_name,
+	category,
+	subcategory,
+	cost,
+	last_sale_date,
+	DATEDIFF(MONTH, last_sale_date, GETDATE()) AS recency_in_months,
+	CASE
+		WHEN total_sales > 50000 THEN 'High-Performance'
+		WHEN total_sales >= 10000 THEN 'Mid-Range'
+		ELSE 'Low-Performance'
+	END AS product_segment,
+	lifespan,
+	total_orders,
+	total_sales,
+	total_quantity,
+	total_customers,
+	avg_selling_price,
+	CASE
+		WHEN total_orders = 0 THEN 0
+		ELSE total_sales / total_orders
+	END AS avg_order_revenue,
+	CASE
+		WHEN lifespan = 0 THEN total_sales
+		ELSE total_sales / lifespan
+	END AS avg_monthly_revenue
+FROM
+	product_aggregations
